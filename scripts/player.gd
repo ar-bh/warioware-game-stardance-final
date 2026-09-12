69fc0dd26d461b8808b6c4f4f0037159cc2739b7
web <146587835+ar-bh@users.noreply.github.com>
2026-09-12 09:46:26 -0400

    Initial commit

---
+++ scripts/player.gd
@@ -0,0 +1,30 @@
+class_name Player
+extends CharacterBody2D
+
+@export var speed := 300.0
+@export var jump_velocity := -500.0
+
+@onready var skin: AnimatedSPrite2D = $Skin
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+	
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * speed
+		skin.flip_h = direction < 0
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+	
+	if not is_on_floor():
+		skin.play("jump")
+	elif direction:
+		animated_sprite.play("walk")
+	else:
+		animated_sprite.play("idle")
+	
+	move_and_slide()
