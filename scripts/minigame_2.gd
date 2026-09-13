74f1b980ea6596a1ced2fcae5c4b4f3db3e1175a
web <146587835+ar-bh@users.noreply.github.com>
2026-09-13 09:44:30 -0400

    toppings do not spawn on top of each other

--- scripts/minigame_2.gd
+++ scripts/minigame_2.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 const NEED := 5
-const TOPPING_SCENE := preload("res://scenes/topping.tscn")
+const TOPPING_SCENE: PackedScene = preload("res://scenes/topping.tscn")
 const TEXTURES := [
 	preload("res://assets/collectible/toppings/mushroom.png"),
 	preload("res://assets/collectible/toppings/cheese.png"),
@@ -9,6 +9,12 @@ const TEXTURES := [
 	preload("res://assets/collectible/toppings/sausage.png"),
 	preload("res://assets/collectible/toppings/pineapple.png"),
 ]
+
+const SPAWN_LEFT := 280.0
+const SPAWN_RIGHT := 1000.0
+const SPAWN_TOP := 220.0
+const SPAWN_BOTTOM := 520.0
+const SPACING := 140.0
 
 var clicked := 0
 var ended := false
@@ -23,26 +29,29 @@ func _ready() -> void:
 	timer.timed_out.connect(_lose)
 	_spawn()
 
+func _random_pos() -> Vector2:
+	return Vector2(randf_range(SPAWN_LEFT, SPAWN_RIGHT), randf_range(SPAWN_TOP, SPAWN_BOTTOM))
+
 func _spawn() -> void:
 	var used: Array[Vector2] = []
 	for tex in TEXTURES:
 		var topping: Area2D = TOPPING_SCENE.instantiate()
-		var pos := Vector2(randf_range(160.0, 1120.0), randf_range(180.0, 580.0))
+		var pos := _random_pos()
 		for _i in 20:
 			var ok := true
 			for other in used:
-				if pos.distance_to(other) < 140.0:
+				if pos.distance_to(other) < SPACING:
 					ok = false
 					break
 			if ok:
 				break
-			pos = Vector2(randf_range(160.0, 1120.0), randf_range(180.0, 580.0))
+			pos = _random_pos()
 		used.append(pos)
 		topping.position = pos
 		topping.get_node("Sprite").texture = tex
 		topping.collected.connect(_on_topping)
 		$Toppings.add_child(topping)
-		
+
 func _on_topping() -> void:
 	clicked += 1
 	if clicked >= NEED:
